3192bada3bc3326af2f0ec700cfe80740ca2196c
Christopher Richez <crichez@pm.me>
2022-05-13 13:04:57 -0500

    Added minimum version requirements for Apple platforms.

--- Package.swift
+++ Package.swift
@@ -7,6 +7,9 @@ let package = Package(
     name: "Logbook",
     platforms: [
         .macOS(.v11),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v8),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
